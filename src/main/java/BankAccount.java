4a1831f235de2c6edf4125569f485545d6c26e22
Jack Chen <23hchen@athenian.org>
2021-10-26 11:56:11 -0700

    working

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,6 +1,36 @@
 public class BankAccount {
-    // Instance variables
+    // state instance variable
     private int balance;
     private int accountNumber;
+    private double interest;
     private String preferredName;
+    //conductor
+    public BankAccount (int b, int a, String p){
+        balance = b;
+        accountNumber = a;
+        preferredName = p;
+    }
+    //behavior
+    public void withdraw(int x){
+        this.balance -= x;
+    }
+    public void deposit(int x){
+        this.balance += x;
+    }
+    public double calculate_interest(){
+        return(this.interest);
+    }
+    public int get_Accountnumber(){
+        return(this.accountNumber);
+    }
+    public String get_PreferredName(){
+        return(this.preferredName);
+    }
+    public void set_PreferredName(String x){
+        this.preferredName = x;
+    }
+    public String to_String(){
+        return("Account number: " + this.accountNumber + "\n Account Balance: " + this.balance);
+
+    }
 }
--- src/main/java/BankClient.java
+++ src/main/java/BankClient.java
@@ -1,12 +1,48 @@
 import java.util.Scanner;
 
 public class BankClient {
-
+//What is your preferred name?
+//Nathan
+//Hi, Nathan! What command would you like to carry out? (withdraw/deposit/check balance/check account number/set preferred name/print summary)
+//check balance
+//Current balance: $0
+//Hi, Nathan! What command would you like to carry out? (withdraw/deposit/check balance/check account number/set preferred name/print summary)
+//deposit
+//How much money would you like to deposit?
+//100
+//You have successfully deposited: $100
+//Hi, Nathan! What command would you like to carry out? (withdraw/deposit/check balance/check account number/set preferred name/print summary)
+//check balance
+//Current balance: $100
+//Hi, Nathan! What command would you like to carry out? (withdraw/deposit/check balance/check account number/set preferred name/print summary)
+//withdraw
+//How much money would you like to withdraw?
+//50
+//You have successfully withdrawn: $50
+//Hi, Nathan! What command would you like to carry out? (withdraw/deposit/check balance/check account number/set preferred name/print summary)
+//check balance
+//Current balance: $50
+//Hi, Nathan! What command would you like to carry out? (withdraw/deposit/check balance/check account number/set preferred name/print summary)
+//check account number
+//Account number: 101134
+//Hi, Nathan! What command would you like to carry out? (withdraw/deposit/check balance/check account number/set preferred name/print summary)
+//set preferred name
+//What would you like your preferred name to be?
+//Nathan Lin
+//Name changed successfully to: Nathan Lin
+//Hi, Nathan Lin! What command would you like to carry out? (withdraw/deposit/check balance/check account number/set preferred name/print summary)
+//print summary
+//Account number 101134 has a balance of: $50
+//Hi, Nathan Lin! What command would you like to carry out? (withdraw/deposit/check balance/check account number/set preferred name/print summary)
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("What is your preferred name?");
         String name = scan.nextLine();
-//        // Create a bank account with the given name and the account number 101134
-//        BankAccount account = new BankAccount(name, 101134);
+        BankAccount account = new BankAccount(0, 101134, name);
+        System.out.println("Hi, " + name + "! Your account number is 101134. What command would you like to carry out? (withdraw/deposit/check balance/check account number/set preferred name/print summary"");
+        );
+        String x = scan.nextLine();
+
+
     }
 }
